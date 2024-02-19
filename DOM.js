24ec429e7d878fc2ab3f9cba6bc5b75658aada45
OMAR FARUK <omarfaruk34552@gmail.com>
2024-02-19 15:30:24 +0600

    select seat and sit color

--- DOM.js
+++ DOM.js
@@ -12,6 +12,41 @@ ticketSection.onclick = function(){
 };
 
 
+// clic ticket button function
+let selectedSeats = 0 ;
+let seats = document.querySelectorAll('.busseat');
 
+seats.forEach((seat) => {
+    seat.addEventListener("click", function() {
+        let isSelected = seat.classList.contains("selected");
+        // console.log(isSelected);
 
+        if (!isSelected && selectedSeats < 4) {
+            seat.classList.add("selected");
+            seat.classList.add("bg-lime-600");
 
+            selectedSeats++;
+        }
+
+        else if (isSelected) {
+            seat.classList.remove("selected");
+            seat.classList.remove("bg-lime-600");
+
+            selectedSeats--;
+        }
+        if (selectedSeats == 4) {
+            seats.forEach(btn => {
+                if (!btn.classList.contains("selected")) {
+                    btn.disable = true;
+                }
+            });
+        }
+        else {
+            seats.forEach(btn => {
+                btn.disable = false;
+            });
+        }
+        console.log(selectedSeats);
+    });
+    
+});
